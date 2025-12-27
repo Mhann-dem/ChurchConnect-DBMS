8117bdf5ad0dabff48ef8c30dc8f3d8c4bffff43
Robert Dzaka <robbddzaka1@gmail.com>
2025-12-27 03:48:03 +0000

    Updating the so to connect to the backend

--- backend/churchconnect/settings.py
+++ backend/churchconnect/settings.py
@@ -181,6 +181,8 @@ FILE_UPLOAD_PERMISSIONS = 0o644
 # SECURITY: CORS settings - restrictive by default
 if DEBUG:
     CORS_ALLOWED_ORIGINS = [
+        "https://churchconnect-dbms-production.up.railway.app"
+        "https://thorough-adventure-production.up.railway.app"
         "http://localhost:3000",
         "https://localhost:3000",
         "http://127.0.0.1:3000",
